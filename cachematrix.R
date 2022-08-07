d51af6cb2ae1022c8d62c2442f4a127a60d2cca7
axemalt <moonblast3933@gmail.com>
2022-08-07 14:06:37 -0700

    added solution

--- cachematrix.R
+++ cachematrix.R
@@ -9,7 +9,7 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setinverse <- function(inverse) i <<- inverse
   getinverse <- function() i
-  list(set = set, get = get, setinverse = setmean, getinverse = getmean)
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
